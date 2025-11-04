e2194d98767121e5e459e421afd5c8cea341ee6d
devoozdevu22 <devoozdevu22@gmail.com>
2025-11-04 09:32:22 +0530

    d10hw

---
+++ d10hw.sql
@@ -0,0 +1,11 @@
+CREATE TABLE authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(100),
+    email VARCHAR(100) UNIQUE
+);
+CREATE TABLE books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(150),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
